657b6581483009516c7606af91833c06ec069a73
Griffin Poole <griffinpoole5@gmail.com>
2019-10-29 15:43:19 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,7 +2,7 @@ class Book
   attr_accessor :author, :page_count
   attr_reader :title, :genre
 
-  GENERES = []
+  GENRES = []
   
   def initialize(title)
     @title = title
@@ -14,7 +14,7 @@ class Book
 
   def genre=(book_genre)
     @genre = book_genre
-    GENERES << book_genre
+    GENRES << book_genre
   end
   
 end
